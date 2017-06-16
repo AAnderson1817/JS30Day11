5e522337b9420bf1f787448621f90429a570de3a
Andy Anderson <andyanderson1818@gmail.com>
2017-06-16 14:53:02 -0700

    Added ff functionality and scrub for video time

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('progress__filled');
+const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
@@ -23,11 +23,35 @@ function updateButton(){
 function skip(){
   video.currentTime += parseFloat(this.dataset.skip);
 }
+//Because our names match up with the actual video property names, we can do the code below.
+function handleRangeUpdate(){
+  video[this.name]=this.value;
+  console.log(this.value);
+}
 
+function handleProgress(){
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e){
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+  console.log(e);
+}
 //Event Listeners
 video.addEventListener('click',togglePlay);
 video.addEventListener('play',updateButton);
 video.addEventListener('pause',updateButton);
+video.addEventListener('timeupdate',handleProgress);
 
 toggle.addEventListener('click',togglePlay);
 skipButtons.forEach(button => button.addEventListener('click',skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click',scrub);
+progress.addEventListener('mousemove',scrub);
+progress.addEventListener('mousedown',() => mousedown = true));
+progress.addEventListener('mouseup',() => mousedown = false));
